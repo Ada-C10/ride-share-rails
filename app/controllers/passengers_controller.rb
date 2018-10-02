5c154441a33277044aaa680c02e7661ba931731e
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-02 11:12:24 -0700

    Created methods for passenger controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,22 +1,52 @@
 class PassengersController < ApplicationController
   def index
+    @passengers = Passenger.all.order(:name)
   end
 
   def show
+    id = params[:id].to_i
+    @passenger = Passenger.find_by(id: id)
+
+    if @passenger.nil?
+      render :notfound, status: :not_found
+    end
   end
 
   def edit
+    @passenger = Passenger.find(params[:id].to_i)
   end
 
   def create
+    ###### TODO Add error message for if it does not save  #####
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
+    @passenger = Passenger.new
   end
 
   def destroy
+    passenger = Passenger.find_by(id: params[:id].to_i)
+    @deleted_passenger = passenger.destroy
+    redirect_to root_path
   end
 
   def update
+    passenger = Passenger.find_by(id: params[:id].to_i)
+    passenger.update(passenger_params)
+    ###### TODO Add error message for if it does not save  #####
+    redirect_to passenger_index_path(passenger.id)
   end
+
+private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
