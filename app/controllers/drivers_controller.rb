1deb7863db4bda51384508da747bf6c2a01fb87f
Daniela Di Iorio <danielad@gmail.com>
2018-10-02 10:31:56 -0700

    basic setup of drivers controller, fixed routing

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,30 +1,52 @@
 class DriversController < ApplicationController
+  before_action :set_driver, only: [:show, :edit, :update, :destroy]
 
   def index
+    @drivers = Driver.all
   end
 
   def show
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @driver.update(driver_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if Task.destroy(params[:id].to_i)
+      redirect_to root_path
+    end
   end
-  
+
 
   private
 
   def driver_params
     return params.require(:driver).permit(:name, :vin)
   end
+
+  def set_driver
+    @driver = Driver.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'drivers/edit'
   root 'drivers#index'
 
-  resources :driver
-  resources :passenger
+  resources :drivers
+  resources :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
