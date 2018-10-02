c0a556d98e60714f7cc4a16e23d279e6e99deaaf
Katie Jahanmir <katietj@gmail.com>
2018-10-02 11:30:22 -0700

    added driver and passenger show and index pages

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -4,6 +4,13 @@ class DriversController < ApplicationController
   end
 
   def show
+    id = params[:id].to_i
+    @driver = Driver.find_by(id: id)
+
+
+    if @driver.nil?
+     render :notfound, status: :not_found
+    end
   end
 
   def edit
