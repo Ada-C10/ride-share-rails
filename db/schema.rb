72fbd62aa1f05efb944eb709eccab5ac83b641f1
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-02 14:16:42 -0700

    Change status to is_available? for readability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_000449) do
+ActiveRecord::Schema.define(version: 2018_10_02_210309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_000449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
-    t.boolean "status"
+    t.boolean "is_available?", default: true
   end
 
   create_table "passengers", force: :cascade do |t|
