65d87fbff52d3bb90c785c7eda0ea93563947ea6
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-02 11:27:39 -0700

    Remove extra comma

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true, format: {with: /\A[a-zA-Z\.\'\-]{2,50}(?: [a-zA-Z\.\'\-]{2,50})+\Z/}
-  validates :vin, presence: true, , format: {with: /[A-HJ-NPR-Z0-9]/i}, length: { is: 17 }
+  validates :vin, presence: true, format: {with: /[A-HJ-NPR-Z0-9]/i}, length: { is: 17 }
 end
