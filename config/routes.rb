159f474ab27ea9df54554d3358eb24a4a6da91ea
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-02 14:17:24 -0700

    Add route to patch driver available status

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :passengers do
     resources :trips, only: [:create]
   end
+
+  patch 'drivers/:id/change_status', to: 'drivers#change_status', as: 'change_status'
 end
