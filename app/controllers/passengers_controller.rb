f2ab29f454740b7a5df3944d464104884df0c1ba
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-02 16:00:13 -0700

    Partial view and form for passengers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -15,42 +15,41 @@ class PassengersController < ApplicationController
   end
 
   def create
-    filtered_passenger_params = passenger_params
-    @passenger = Passenger.new(filtered_passenger_params)
+    @passenger = Passenger.new(passenger_params)
 
     if @passenger.save
       redirect_to passengers_path
     else
       render :new
     end
+  end
 
-    def edit
-      @passenger = Passenger.find_by(id: params[:id])
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+  end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    if passenger.update(passenger_params)
+      redirect_to passenger_path(passenger.id)
+    else
+      head :not_acceptable
     end
+  end
 
-    def update
-      passenger = Passenger.find(params[:id])
-      if passenger.update(passenger_params)
-        redirect_to passenger_path(passenger.id)
-      else
-        head :not_acceptable
-      end
-    end
+  def destroy
+    passenger = Passenger.find_by(id: params[:id])
 
-    def destroy
-      passenger = Passenger.find_by(id: params[:id])
+    passenger.destroy
+    redirect_to passengers_path
+  end
 
-      passenger.destroy
-      redirect_to passengers_path
-    end
+  private
 
-    private
-
-    def passenger_params
-      return params.require(:passenger).permit(
-        :name,
-        :phone_num
-      )
-    end
+  def passenger_params
+    return params.require(:passenger).permit(
+      :name,
+      :phone_num
+    )
   end
 end
