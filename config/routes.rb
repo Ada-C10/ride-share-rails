8001211d75979dfbcaa2f376f03db3e287976b64
Addison Cole <addison.cole85@gmail.com>
2018-10-02 12:20:59 -0700

    added controller for passengers and index view. added routes for passengers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get '/passengers', to: 'passengers#index', as: 'passengers'
+
+get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
+post '/passengers', to: 'passengers#create'
+
+get '/passengers/:id', to: 'passengers#show', as: 'passenger'
+
+patch '/passengers/:id', to: 'passengers#update'
+
+delete '/passengers/:id', to: 'passengers#destroy'
+
+get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
 end
