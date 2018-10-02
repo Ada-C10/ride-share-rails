7d29ea09d8490f5bbf6f76c7d7deff7743ebbb10
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-02 16:21:37 -0700

    Created controls for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,22 +1,52 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all.order(:date)
   end
 
   def show
+    id = params[:id].to_i
+    @trip = Trip.find_by(id: id)
+
+    if @trip.nil?
+      render :not_found, status: :not_found
+    end
   end
 
   def edit
+    @trip = Trip.find(params[:id].to_i)
   end
 
   def create
+    ##### TODO Add error messages for if it does not save ####
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
   def new
+    @trip = Trip.new
   end
 
   def destroy
+    trip = Trip.find_by(id: params[:id].to_i)
+    trip.destroy
+    redirect_to trips_path
   end
 
   def update
+    trip = Trip.find_by(id: params[:id].to_i)
+    trip.update(trip)
+    ###### TODO Add error message for if it does not save  #####
+    redirect_to trip(trip.id)
   end
+
+  private
+
+    def trip_params
+      return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+    end
+
 end
