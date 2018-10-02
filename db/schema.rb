a6b7747b867231079fd69781b624ddbdca9da949
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-02 15:10:06 -0700

    add trip model, controller, references, and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_185623) do
+ActiveRecord::Schema.define(version: 2018_10_02_220227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,18 @@ ActiveRecord::Schema.define(version: 2018_10_02_185623) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "trips", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "rating"
+    t.decimal "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "passenger_id"
+    t.bigint "driver_id"
+    t.index ["driver_id"], name: "index_trips_on_driver_id"
+    t.index ["passenger_id"], name: "index_trips_on_passenger_id"
+  end
+
+  add_foreign_key "trips", "drivers"
+  add_foreign_key "trips", "passengers"
 end
