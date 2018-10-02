8fa1944838cd52bbd4eca52f7fc5072a6e4668a1
Shelan <shelandebesai@gmail.com>
2018-10-02 16:57:48 -0700

    total driver cost

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,4 +4,13 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_revenue
+    total = 0
+    trips.each do |trip|
+
+    total += (trip.cost - 1.65)
+    end
+    return total_revenue = (total * 0.80)
+  end
 end
