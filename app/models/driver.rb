a9716326c57ee19c002101013b6713fac31060f9
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-02 16:34:45 -0700

    average rating, earnings, charges done

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,31 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true, uniqueness: true
+
+  # def earnings
+  #   drivers_trips = self.all(id: @driver.id)
+  #   fee = drivers_trips.length * 1.65 #a fee is subtracted for each trip, multiplied by total trips
+  #   gross_earnings = drivers_trips.cost.reduce(:+) - fee
+  #   net_earnings = gross_earnings * 0.80 #net is 80% of total
+  #
+  #   return net_earnings
+  # end
+
+  def earnings
+    total = 0
+    self.trips.each do |trip|
+      total += trip.cost - 1.65
+    end
+    return (total * 0.80).round(2)
+  end
+
+  def ratings
+    average = 0
+    self.trips.each do |trip|
+      average += trip.rating.to_f #to float to allow for half ratings
+    end
+    rating = average/ (self.trips.length)
+
+    return rating.round(1) #rounded only one space (4.1, 2.5, etc)
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,13 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+  #task is independent of context -- biz logic only
+  def earnings
+    drivers_trips = self.all(id: params[:id].to_i)
+    fee = drivers_trips.length * 1.65 #a fee is subtracted for each trip, multiplied by total trips
+    gross_earnings = drivers_trips.cost.reduce(:+) - fee
+    net_earnings = gross_earnings * 0.80
+    
+    return net_earnings
+  end
 end
