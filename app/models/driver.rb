1a8dd28d914f7454b2946e706f7d4f8a6fe7c9c8
Danielle Metzner <danimetzner@gmail.com>
2018-10-02 15:07:42 -0700

    Added total_earnings method into active record for Driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,10 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earnings
+  #The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
+   total_cost = self.trips.reduce(0) { |sum, trip| sum + trip.cost }
+   num_trips = self.trips.length
+   return (total_cost - (num_trips * 1.65)) * .8
+  end
 end
