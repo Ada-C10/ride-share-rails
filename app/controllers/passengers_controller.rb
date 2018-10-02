8abe0fc6447a651a67195ff6093faed5f38a8f70
Goeun Park <goeuntothepark@gmail.com>
2018-10-02 15:43:40 -0700

    add show method in PassengerController

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,6 +1,16 @@
 class PassengersController < ApplicationController
-  
+
   def index
     @passengers = Passenger.all
   end
+
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.all.find_by(id: passenger_id)
+
+    if @passenger.nil?
+      head :not_found
+    end
+  end
+
 end
