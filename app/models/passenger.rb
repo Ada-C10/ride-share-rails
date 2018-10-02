8951e07d453621cca83ffa7166eea7395cca9a99
Danielle Metzner <danimetzner@gmail.com>
2018-10-02 15:26:08 -0700

    Added total_cost method to Passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,10 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_cost
+  #The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
+   total_cost = self.trips.reduce(0) { |sum, trip| sum + trip.cost }
+   total_cost /= 100.0
+   return total_cost.round(2)
+  end
 end
