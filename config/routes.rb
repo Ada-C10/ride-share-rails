7fd9ad1541028ef39596590b3828335d8e5a9615
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-02 16:54:34 -0700

    add homepage controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#index'
+  root 'homepages#index'
 
-  resources :applications
+  resources :homepages
   resources :trips
   resources :drivers
   resources :passengers
