09d908574b554c4e25bf836bb40cba2bbc1f5a80
Goeun Park <goeuntothepark@gmail.com>
2018-10-02 16:19:12 -0700

    create and update of CRUD of passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -5,12 +5,55 @@ class PassengersController < ApplicationController
   end
 
   def show
-    passenger_id = params[:id]
-    @passenger = Passenger.all.find_by(id: passenger_id)
-
+    @passenger = Passenger.find_by(id: params[:id])
     if @passenger.nil?
       head :not_found
     end
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    result = @passenger.update(passenger_params)
+
+    if result
+      redirect_to passenger_path(@passenger.id)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def destroy
+    passenger = Passenger.find_by(id: params[:id])
+
+    passenger.destroy
+    redirect_to passengers_path
+  end
+
+
+  private
+  def passenger_params
+    return params.require(:passenger).permit(
+      :name,
+      :phone_num
+    )
   end
 
 end
