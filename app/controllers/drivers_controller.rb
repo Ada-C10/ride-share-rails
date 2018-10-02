278cd7f94c1f235edc3ac7bea6258df80171b5b6
Lindsay Terchin <lterchin@gmail.com>
2018-10-02 15:13:34 -0700

    created new methods in the drivers controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,5 +1,53 @@
 class DriversController < ApplicationController
+
   def index
     @drivers = Driver.all.order(:name)
   end
+
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+    else
+      #TODO validation
+      render :new
+    end
+  end
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.update(driver_params)
+      redirect_to driver_path(driver)
+    end
+  end
+
+  def destroy
+
+    @driver = Driver.find_by(id: params[:id])
+    @driver.destroy
+
+    redirect_to drivers_path
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+  
 end
