fac930b11a9d197432aee742652b2e67c40b3c2e
Aurea <fu.sakura@gmail.com>
2018-10-02 11:51:13 -0700

    CRUD controller implementation for passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,22 +1,55 @@
 class PassengersController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def new
+    @passengers = Passenger.all
   end
 
   def show
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+    end
   end
 
-  def update
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    filtered_passenger_params = passenger_params
+    @passenger = Passenger.new(filtered_passenger_params)
+
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new
   end
 
   def edit
+    @passenger = Passenger.find_by(id: params[:id])
+  end
+
+  def update
+    passenger = Passenger.find(params[:id])
+    if passenger.update(passenger_params)
+      redirect_to passenger_path(passenger.id)
+    else
+      head :not_acceptable
+    end
   end
 
   def destroy
+    passenger = Passenger.find_by(id: params[:id])
+
+    passenger.destroy
+    redirect_to passengers_path
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(
+      :name,
+      :phone_num
+    )
   end
 end
