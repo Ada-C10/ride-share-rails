dbd2ac943ce549741d40fe1238a9876bdfb0ae44
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-02 11:15:34 -0700

    added show/index views for passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,11 +1,13 @@
 class PassengersController < ApplicationController
   def show
+    @passenger = Passenger.find_by(id:params[:id])
   end
 
   def new
   end
 
   def index
+    @passengers= Passenger.all
   end
 
   def edit
