ef753bc07b81274328dd8286aa194de4d2de0693
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-02 15:26:51 -0700

    added show and index method in the trips controller along with strong params

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,26 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+
+
+
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(
+      :date,
+      :rating,
+      :cost,
+      :driver_id,
+      :passenger_id
+
+    )
+  end
+
 end
