d9a80ae1a1633284d866495c46ad326294371834
KatherineJF <kjfitzpat@hotmail.com>
2018-10-02 15:52:18 -0700

    CRUD bugfix driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -7,8 +7,38 @@ class DriversController < ApplicationController
     def show
       id = params[:id]
       @driver = Driver.find_by(id:id)
+    end
+
+    def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @driver = Driver.find(params[:id].to_i)
+  end
+
+    def update
+      @driver = Driver.find(params[:id].to_i)
+      @driver.update(driver_params)
+      driver_path(@driver)
 
     end
+
+    def destroy
+      driver = Driver.find_by(id: params[:id].to_i)
+      @deleted_driver = driver.destroy
+    end
+
+    private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :driver_picture)
+  end
 
 
 end
