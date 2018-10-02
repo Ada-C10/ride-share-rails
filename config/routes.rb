a23d12640d6b38f7ac40eb090ca8f5922a175d65
sabine <drsabinethomas@gmail.com>
2018-10-02 15:10:48 -0700

    refactored a coded for passenger

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :trips, :drivers, :passengers 
+  # root - TODO  create a controller and view for the homepage to redirect
+  # after deleting a passenger (passenger list or  homepage?)
+
+  resources :trips, :drivers, :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
