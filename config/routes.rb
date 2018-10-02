7374efac2d772b79188ffe86aa70034674be356d
Addison Cole <addison.cole85@gmail.com>
2018-10-02 16:49:55 -0700

    resolving conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -5,23 +5,4 @@ root 'home#index'
 
 resources :passengers
 resources :drivers
-#resources :trips
-
-# get '/drivers', to: 'drivers#index', as: 'drivers' DONE
-
-# get '/drivers/new', to: 'drivers#new', as: 'new_driver' DONE
-# post '/drivers', to: 'drivers#create' DONE
-
-<<<<<<< HEAD
-# get '/drivers/:id', to: 'drivers#show', as: 'driver' DONE
-# #
-# patch '/drivers/:id', to: 'drivers#update'
-#
-# delete '/drivers/:id', to: 'drivers#destroy'
-#
-# get '/drivers/:id/edit', to: 'drivers#edit', as: 'edit_driver'
-end
-=======
-get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
-end 
->>>>>>> 7413b5e7dccbccc8d39d791966b4b92c2fe91883
+resources :trips
