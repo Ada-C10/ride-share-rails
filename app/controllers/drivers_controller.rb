045ee74031d3258574d35921354e327245a612a2
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-02 11:51:39 -0700

    Driver controller CRUD actions

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,22 +1,56 @@
 class DriversController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def new
+    @drivers = Driver.all
   end
 
   def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+    end
   end
 
-  def update
+  def new
+    @driver = Driver.new
   end
 
-  def edit
-  end
+  def create
+    filtered_driver_params = driver_params
+    @driver = Driver.new(filtered_driver_params)
 
-  def destroy
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+
+    def edit
+      @driver = Driver.find_by(id: params[:id])
+    end
+
+    def update
+      driver = Driver.find(params[:id])
+      if driver.update(driver_params)
+        redirect_to driver_path(driver.id)
+      else
+        head :not_acceptable
+      end
+    end
+
+    def destroy
+      driver = Driver.find_by(id: params[:id])
+
+      driver.destroy
+      redirect_to drivers_path
+    end
+
+    private
+
+    def driver_params
+      return params.require(:driver).permit(
+        :name,
+        :vin
+      )
+    end
   end
 end
