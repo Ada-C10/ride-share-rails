b6af73ee5a05a4f2eeb7c375b07d86481806bb30
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-02 11:20:20 -0700

    Implemented average rating method in driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,10 +3,17 @@ class Driver < ApplicationRecord
 
   def total_earnings
     total = 0
-    all_trips = self.trips
-    all_trips.each do |trip|
+    self.trips.each do |trip|
       total += trip.cost
     end
     return total
+  end
+
+  def average_rating
+    sum = 0
+    self.trips.each do |trip|
+      sum += trip.rating
+    end
+    return (sum / self.trips.count).round(2)
   end
 end
