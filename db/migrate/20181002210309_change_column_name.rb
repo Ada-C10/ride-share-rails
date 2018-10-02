e84cc72ff2e5f29bd13dc9b8e68f207cf5a44134
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-02 14:18:21 -0700

    Change status to is_available? for readability

---
+++ db/migrate/20181002210309_change_column_name.rb
@@ -0,0 +1,6 @@
+class ChangeColumnName < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :drivers, :status, :is_available?
+    change_column :drivers, :is_available?, :boolean, default: true
+  end
+end
