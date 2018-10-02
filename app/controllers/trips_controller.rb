f083816656a295499b864796b0efaf2ac4d09081
Karis Kim <ouniiq@hotmail.com>
2018-10-02 16:05:31 -0700

    filled out all methods for trips and passengers controllers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,22 +1,56 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all
   end
 
   def show
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+    end
   end
 
   def new
+    @trip = Trip.new
   end
 
   def create
+    filtered_trip_params = trip_params()
+    @trip = Trip.new(filtered_trip_params)
+
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
   end
 
   def update
+    trip = Trip.find(params[:id])
+    trip.update(trip_params)
+
+    redirect_to trip_path(trip.id)
   end
 
   def destroy
+    trip = Trip.find_by(id: params[:id])
+
+    trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(
+      :date,
+      :rating,
+      :cost
+    )
   end
 end
