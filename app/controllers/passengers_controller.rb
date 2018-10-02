df6fd86f4f77b14441285ef848dd38814d67bbd5
Maryam Shitu <maryamshitu@gmail.com>
2018-10-02 11:15:51 -0700

    created passenger views

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,17 @@
+require 'time'
+
 class PassengersController < ApplicationController
+
+  def index
+    @passengers = Passenger.all  #..order(:title)
+  end
+
+  def show
+    @passengers = Passenger.find_by(id: params[:id].to_i)
+
+    if @passengers.nil?
+      render :notfound, status: :not_found
+    end
+  end
+
 end
