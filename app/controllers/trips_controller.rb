64fa67d5db4da85c4af8bbdb6ef0433a072ebfb5
Aurea <fu.sakura@gmail.com>
2018-10-02 11:44:29 -0700

    controller CRUD implementation for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,23 +1,60 @@
 class TripsController < ApplicationController
 
   def index
-  end
-
-  def create
-  end
-
-  def new
+    @trips = Trip.all
   end
 
   def show
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+    end
   end
 
-  def update
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    filtered_trip_params = trip_params
+    @trip = Trip.new(filtered_trip_params)
+
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
   end
 
+  def update
+    trip = Trip.find(params[:id])
+    if trip.update(trip_params)
+      redirect_to trip_path(trip.id)
+    else
+      head :not_acceptable
+    end
+  end
+
+
   def destroy
+    trip = Trip.find_by(id: params[:id])
+
+    trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(
+      :driver_id,
+      :passenger_id,
+      :date,
+      :rating,
+      :cost
+    )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_235515) do
+ActiveRecord::Schema.define(version: 2018_10_02_000109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2018_10_01_235515) do
   create_table "drivers", force: :cascade do |t|
     t.string "name"
     t.string "VIN"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
