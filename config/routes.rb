bdf2ce561325a230d625c05aa0e80da5d35b2b21
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-02 11:51:19 -0700

    added 3 resources (7 restful routes for all 3 controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'trips#index'
+
+    # Auto-generate all the RESTful routes
+    resources :trips
+    resources :drivers
+    resources :passengers
+
 end
