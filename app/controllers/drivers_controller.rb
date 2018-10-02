80d6c020989218dac8a3c2f921b20e0c3e3b03e0
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-02 15:37:05 -0700

    added driver show and index

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,11 @@
+class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    # if @driver.nil?
+    # 
+    # end
+  end
+end
