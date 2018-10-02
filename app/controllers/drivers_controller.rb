a72b9ce715acd41ba55b4b17c7f27637cc22571c
Laura <larmartinez2@gmail.com>
2018-10-02 15:37:21 -0700

    make create and new actions

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,3 +1,21 @@
 class DriversController < ApplicationController
   # create routes
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
