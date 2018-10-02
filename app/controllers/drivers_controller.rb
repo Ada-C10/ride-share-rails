c6ee6d852631faad1b557001c5ce9aa47f03e3f0
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-02 14:46:32 -0700

    added driver controller methods create/update

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -8,8 +8,40 @@ class DriversController < ApplicationController
   end
 
   def new
+    @driver = Driver.new
   end
 
-  def edit
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
   end
+
+  def update
+    @driver = Driver.find_by(id:params[:id])
+    @driver.update(driver_params)
+    if @driver.save
+      redirect_to driver_path
+    else
+      render :new
+    end
+  end
+
+
+
+  def edit
+    @driver = Driver.find_by(id:params[:id])
+  end
+
+
+
+private
+
+def driver_params
+  return params.require(:driver).permit(:name, :vin)
+end
+
 end
