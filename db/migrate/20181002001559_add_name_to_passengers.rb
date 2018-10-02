b99d2ee31f9a8d5a4500e8fb1061047c8ba1662d
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-01 17:39:13 -0700

    successfully added passenger table

--- db/migrate/20181002001559_add_name_to_passengers.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToPassengers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :passengers, :name, :string
-  end
-end
--- db/migrate/20181002001933_add_phone_num_to_passengers.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneNumToPassengers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :passengers, :phone_num, :string
-  end
-end
