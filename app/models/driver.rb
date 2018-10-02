aa5b70cd8350396ea7873aa9939539e8e19ef053
Daniela Di Iorio <danielad@gmail.com>
2018-10-02 11:41:19 -0700

    driver model calculates total earnings and average rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,22 @@ class Driver < ApplicationRecord
   validates :vin, presence: true, uniqueness: true
 
   has_many :trips
+
+  def total_earnings
+    total = 0
+    self.trips.each do |trip|
+      total += trip.cost
+    end
+    return total
+  end
+
+  def average_rating
+    sum = 0
+    self.trips.each do |trip|
+      sum += trip.rating
+    end
+    return (sum.to_f / self.trips.length).round(1)
+  end
+
+
 end
