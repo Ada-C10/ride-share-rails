0a4d1dd4b6e1b9709f5bb1f835b979253e5bf650
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-02 13:29:57 -0700

    passenger#total_cost built, needs formatting

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,10 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_cost
+    total_cost = self.trips.reduce(0) { |sum, trip| sum + trip.cost }
+
+    return total_cost
+  end
+
 end
