491d3d2051543ea80b0d52fdb53991e55c83e842
Danielle Metzner <danimetzner@gmail.com>
2018-10-02 15:21:22 -0700

    Added average rating for driver. Woohoo

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,6 +5,13 @@ class Driver < ApplicationRecord
   #The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
    total_cost = self.trips.reduce(0) { |sum, trip| sum + trip.cost }
    num_trips = self.trips.length
-   return (total_cost - (num_trips * 1.65)) * .8
+   total_earnings = ((total_cost - (num_trips * 1.65)) * 0.8) / 100
+   return total_earnings.round(2)
+  end
+
+  def average_rating
+    sum_rating = self.trips.reduce(0) { |sum, trip| sum + trip.rating }
+    num_trips = self.trips.length.to_f
+    return (sum_rating/num_trips).round(2)
   end
 end
