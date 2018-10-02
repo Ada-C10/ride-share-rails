4f65605371d319df0648f0db6e7cde70182786f5
Naheed Arang <arang.na@gmail.com>
2018-10-02 16:41:26 -0700

    change phone_num from integer to string

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -4,17 +4,31 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
+  def edit
+  end
 
+  def new
+  end
 
+  def destroy
+  end
 
+  def create
+  end
 
+  def show
+  end
 
+  def update
+  end
 
+  private
 
-
-
-
-
-
+  def book_params
+    return params.require(:driver).permit(
+      :name,
+      :vin
+    )
+  end
 
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,6 +4,34 @@ class PassengersController < ApplicationController
     @passengers = Passengers.all
   end
 
+  def edit
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  private
+
+  def book_params
+    return params.require(:passenger).permit(
+      :name,
+      :phone_num,
+    )
+  end
+
+
 
 
 
