95fef23e509d43c28c1b7ef9fcc1b873df71f4b6
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-02 15:16:37 -0700

    Driver vin column migration and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_215233) do
+ActiveRecord::Schema.define(version: 2018_10_02_220359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "drivers", force: :cascade do |t|
     t.string "name"
-    t.string "VIN"
+    t.string "vin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
