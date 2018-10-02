980d8894c4d4d925a94a594cd69cf31c3928c0ec
Hannah <hannahvanseeters@gmail.com>
2018-10-02 16:03:32 -0700

    average rating and total earnings mthods created and in driver view window

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,9 +1,23 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+  def trip_cost
+  end
+
   def total_earnings
+    total = 0
+    self.trips.each do |trip|
+      trip_price = (trip.cost - 1.65) * 0.80
+      total += trip_price
+    end
+    return total
   end
 
   def average_rating
+    total = 0
+    self.trips.each do |trip|
+      total += trip.rating
+    end
+    return (total.to_f / self.trips.length).round(2)
   end
 end
