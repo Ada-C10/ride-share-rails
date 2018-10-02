e4b226a4014bada5ceb6fa861a0192f2b72e1ab7
Trang <trang.frego@gmail.com>
2018-10-02 16:06:10 -0700

    add trips show action and trips show view

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,32 @@
 class TripsController < ApplicationController
   def show
+    @trip = Trip.find_by(id: params[:id].to_i)
   end
 
-  def index
-  end
+  # def index
+  #   @trips = Trip.all
+  # end
 
   def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip= Trip.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id].to_i)
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:driver_id, :passenger_id, :date, :rating, :cost)
   end
 end
