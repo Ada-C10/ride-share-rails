cda670e13baa9fa2c915f8fceb01f4fcb9655c82
Amber Lynn <griffiam@gmail.com>
2018-10-02 15:42:20 -0700

    Added create new passenger view and form

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -9,4 +9,19 @@ class PassengersController < ApplicationController
       head :not_found
     end
   end
-end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(name: params[:passenger][:name],
+                          phone_num: params[:passenger][:phone_num]) #instantiate a new passenger
+
+      if @passenger.save # save returns true if the database insert succeeds
+        redirect_to all_passengers_path # go to the index so we can see the passenger in the list
+      else # save failed :(
+        render :new # show the new passenger form view again
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
 
   get '/passengers', to: 'passengers#index', as: 'all_passengers'
 
+  get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
+  post "/passengers", to: "passengers#create"
+
+
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
+  patch '/passengers/:id', to: 'tasks#update'
+
+
+
   get '/passengers/:id', to: 'passengers#show', as: 'passenger'
 end
