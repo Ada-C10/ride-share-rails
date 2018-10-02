b2b42d3650142b35b5ac4d8b6c0950d5a459286e
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-02 11:16:23 -0700

    Added total earnings method to driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,12 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earnings
+    total = 0
+    all_trips = self.trips
+    all_trips.each do |trip|
+      total += trip.cost
+    end
+    return total
+  end
 end
