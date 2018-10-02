670e7602402c0808c319bfb42d94bcc151a1edc5
Trang <trang.frego@gmail.com>
2018-10-02 15:16:29 -0700

    created total_earned method for Drivers

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,10 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_earned
+    before_fees = (self.trips.reduce(0.0) { |sum, trip| sum + trip.cost }) / 100
+    total = (before_fees - (self.trips.length * 1.65)) * 0.8
+
+    return total.round(2)
+  end
 end
