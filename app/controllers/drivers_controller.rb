3ee4c7f2703f61568ae4ff6470d95f2c1e9b71af
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-02 11:50:02 -0700

    can add new driver, new passenger

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -17,5 +17,23 @@ class DriversController < ApplicationController
   end
 
   def new
+    @driver = Driver.new
   end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
+
 end
