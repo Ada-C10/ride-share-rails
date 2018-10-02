d63095d02c96fef2e06e03affbe85f38d2ecc976
sabine <drsabinethomas@gmail.com>
2018-10-02 10:45:34 -0700

    Created driver controller and empty views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'drivers/index'
+  get 'drivers/show'
+  get 'drivers/edit'
+  get 'drivers/create'
+  get 'drivers/new'
+  get 'drivers/destroy'
+  get 'drivers/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
