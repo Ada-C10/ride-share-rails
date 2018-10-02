28fd8d6e32ceedcd0244a97e02bedab7d024d520
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-02 16:17:33 -0700

    added show method with views.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,11 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.all
   end
+
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+    end
+  end
 end
