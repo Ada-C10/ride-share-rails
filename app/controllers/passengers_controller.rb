cc1b74454a0ef16d14005a285f0845f0a54b72f6
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-02 10:48:30 -0700

    added CRUD, new, index, and show methods to Passenger controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,19 +1,57 @@
 class PassengersController < ApplicationController
+  before_action :get_passenger, only: [:edit, :update, :destroy]
+  def get_passenger
+    @passenger = Passenger.find(params[:id].to_i)
+  end
+
+  
   def index
+    @Passengers = Passenger.all
   end
 
   def show
+    id = params[:id].to_i
+    @passenger = Passenger.find_by(id: id)
+    if @passenger.nil?
+      render :notfound, status: :not_found
+    end
   end
 
   def edit
   end
 
-  def destroyupdate
+  def destroy
+    @passenger.destroy
+    # redirect_to passenger_root_path
+  end
+    
+  def update
+    if @passenger.update(passenger_params)
+      # redirect_to 
+    else
+      render :new
+    end
   end
 
+
   def new
+    @passenger = Passenger.new
   end
 
   def create
+    if @passenger.save(passenger_params)
+      # redirect_to 
+    else
+      render :new
+    end
+  end
+
+  private # only accessible within the model's controller
+  def passenger_params
+    # provide the elements that can be passed in as parameters
+    # this makes strong params allow for DRY and secure
+    return params.require(:passenger).permit(:name,
+                                        :phone_num)
   end
 end
+
