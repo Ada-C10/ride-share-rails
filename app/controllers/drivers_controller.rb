e8e75e48f769059cc6477b634a210eddf04143c7
Laura <larmartinez2@gmail.com>
2018-10-02 17:06:44 -0700

    make destroy action using rescue

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -26,6 +26,11 @@ class DriversController < ApplicationController
   end
 
   def destroy
+    # we should actually make them inactive instead of deleting them
+    # you should delete them if they are are a bad driver:
+    # options: delete it's associated trips, but then the passangers can't see their history of trips
+    # better option to make drivers inactive by adding an status column 
+
     driver = Driver.find_by(id: params[:id].to_i)
     # infom user: you tried to delete a driver who has a trip (currently driving) , you can't delete it because of foreing key currently attached to trip
     # destroy action throws an error exception, so you can't use if statement, you have to rescue that exception
