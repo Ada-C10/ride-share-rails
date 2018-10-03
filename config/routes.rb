60a59e8c3fbb6cf6c6d6b34f1d079c79514b80c2
Trang <trang.frego@gmail.com>
2018-10-03 14:49:02 -0700

    update with nested trips routes and update trips index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root 'passengers#index'
   # root 'Passengers#index'
-  resources :passengers
-  resources :drivers
+  resources :passengers do
+    resources :trips, only: [:index, :new, :create]
+  end
+
+  resources :drivers do
+    resources :trips, only: [:index]
+  end
+
   resources :trips
 
 end
