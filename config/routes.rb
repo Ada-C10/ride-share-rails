3fc347bc80af9afb9a33867fbbb4bd95656cfb2b
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-03 15:22:44 -0700

    Created nested passenger/trips routes and moved passenger show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'trips#index'
-  resources :trips, except: [:new]
+  resources :trips, except: [:create, :new]
   resources :drivers
-  resources :passengers
+  resources :passengers, except: [:show] do
+    resources :trips, only: [:index, :show, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
