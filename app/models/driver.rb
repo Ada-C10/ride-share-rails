7646bf134ce6285b580098157c70f37a6bac2cb8
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-03 11:24:26 -0700

    Created nested routes for passengers/drivers to be linked to trips

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,5 +2,21 @@ class Driver < ApplicationRecord
   has_many :trips
 
   validates :name, presence: true
-  validates :vin, presence: true 
+  validates :vin, presence: true
+
+  # def amount_earned
+  #   sum = 0
+  #   self.trips.each do |trip|
+  #     sum += cost
+  #   end
+  #   return (sum - 1.65) * 0.80
+  # end
+
+  # Get amount driver earned
+  # driver where id = driver.id
+  # sum = 0
+  # driver.trips.each do |trip|
+    # sum += cost
+    # end
+    # minus a fee, multiply by .80
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   # after deleting a passenger (passenger list or  homepage?)
 
   resources :trips, :drivers, :passengers
+
+  # Nested routes for drivers and passengers to have access to trips
+  resources :drivers do
+    resources :trips, only: [:index, :new]
+  end
+
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
