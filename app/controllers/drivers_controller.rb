41c1bc4336c03d05c97b37ad6e967dda905bbae6
Laura <larmartinez2@gmail.com>
2018-10-02 17:31:43 -0700

    allow user to delete driver (inactive)

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -29,17 +29,18 @@ class DriversController < ApplicationController
     # we should actually make them inactive instead of deleting them
     # you should delete them if they are are a bad driver:
     # options: delete it's associated trips, but then the passangers can't see their history of trips
-    # better option to make drivers inactive by adding an status column 
+    # better option to make drivers inactive by adding an status column
 
     driver = Driver.find_by(id: params[:id].to_i)
     # infom user: you tried to delete a driver who has a trip (currently driving) , you can't delete it because of foreing key currently attached to trip
     # destroy action throws an error exception, so you can't use if statement, you have to rescue that exception
     begin
       driver.destroy
-      redirect_to drivers_path
     rescue
-      render :deletewarning
+      driver.status = false
+      driver.save
     end
+    redirect_to drivers_path
   end
 
   private
