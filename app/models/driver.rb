b8e6191ade1d084ccf3fc4d4207cf300ba56f0fa
KatherineJF <kjfitzpat@hotmail.com>
2018-10-02 19:18:37 -0700

    available driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,6 +2,6 @@ class Driver < ApplicationRecord
   has_many :trips
 
   def self.next_available
-    return Driver.first
+    return Driver.where.not(id: Trip.where(rating: nil).select(:driver_id)).order("RANDOM()").first
   end
 end
