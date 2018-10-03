2b70bdd031694575f9d63d00e5f2372a6e997d4c
Carly Reiter <yepodacarly@gmail.com>
2018-10-03 05:20:33 -0700

    Added validations to driver controller and edit and new views.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -48,9 +48,12 @@ class DriversController < ApplicationController
   # can't delete, as "ERROR:  update or delete on table "drivers" violates foreign key constraint "fk_rails_e7560abc33" on table "trips"
   #DETAIL:  Key (id)=(2) is still referenced from table "trips"."
 
+#According to Dee, can modify trips within the Driver ID.  Changed driver 9 name and vin to "Driver has left the system.  Change driver of all of the trips driven by the deleted driver to driver 9?"
+
   def destroy
     driver = Driver.find_by(id: params[:id])
     driver.destroy
+
     redirect_to drivers_path
   end
 
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,7 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true
+  validates :vin, uniqueness: true
+  
 end
