34449226aa361f5c28fc33e254c1e303c55acf8d
Maryam Shitu <maryamshitu@gmail.com>
2018-10-02 17:00:44 -0700

    ride index and show added

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,14 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find(params[:id].to_i)
+
+    if @trip.nil?
+     render :notfound, status: :not_found
+    end
+  end
+  
 end
