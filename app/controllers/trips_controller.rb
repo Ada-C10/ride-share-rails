bfff8ab8b458b18a183ecd2f8afb0737f661f6c5
KatherineJF <kjfitzpat@hotmail.com>
2018-10-02 18:56:25 -0700

    added request ride to passenger

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,19 @@ class TripsController < ApplicationController
     @trips = Trip.all
   end
 
+  def create
+    trip = Trip.new(trip_params)
+    trip.cost = rand(100...5000)
+    trip.date = Time.now
+    trip.driver = Driver.next_available
+
+    if trip.save
+      redirect_to passenger_path(trip.passenger.id)
+    else
+      #redirect to passenger page with error no available drivers
+    end
+  end
+
   def show
     @trip = Trip.find(params[:id].to_i)
 
@@ -10,5 +23,12 @@ class TripsController < ApplicationController
      render :notfound, status: :not_found
     end
   end
-  
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:passenger_id)
+  end
+
+
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,7 @@
 class Driver < ApplicationRecord
-  has_many :trips 
+  has_many :trips
+
+  def self.next_available
+    return Driver.first
+  end
 end
