c7502255f4922fd88eeb96ce6b6855d709b53950
Carly Reiter <yepodacarly@gmail.com>
2018-10-03 16:48:36 -0700

    Added nested routesn and controller logic for trip/driver and trip/passenger.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,15 @@
 class TripsController < ApplicationController
 
   def index
-    @trips = Trip.all
+    if params[:driver_id]
+      driver = Driver.find_by(id: params[:driver_id])
+      @trips = driver.trips
+    elsif params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id])
+      @trips = passenger.trips
+    else
+      @trips = Trip.all
+    end
   end
 
   def show
@@ -12,7 +20,15 @@ class TripsController < ApplicationController
   end
 
   def new
-    @trip = Trip.new
+    if params[:driver_id]
+      driver = Driver.find_by(id: params[:driver_id])
+      @trip = driver.trips.new
+    elsif params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id])
+      @trip = passenger.trips.new
+    else
+      @trip = Trip.new
+    end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
   resources :trips
   resources :passengers
 
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
+
+  resources :drivers do
+    resources :trips, only: [:index, :new]
+  end
+
 end
