306b3a549d3e39b43677667b17da6b1657542f33
Divya <divyamohanan@gmail.com>
2018-10-02 23:46:27 -0700

    Some functions for passenger working

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,8 +3,49 @@ class PassengersController < ApplicationController
     @passengers = Passenger.all
   end
 
-  # def show
-  #   id = params[:id].to_i
-  #   @passenger = Passenger.find_by(id: id)
-  # end
+  def show
+    id = params[:id].to_i
+    @passenger = Passenger.find_by(id: id)
+    #@driver = Driver.find_by(id: params[:id])
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      puts "#{@passengers.errors}"
+      render :new
+    end
+  end
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id].to_i)
+    @passenger.update(passenger_params)
+
+    redirect_to passenger_path(@passenger.id)
+  end
+
+
+  def destroy
+    passenger = Passenger.find_by(id: params[:id].to_i)
+    passenger.destroy
+
+    redirect_to passengers_path
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
