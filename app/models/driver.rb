14e23b290928f70e2740c4a555fdca6f9d18f333
Shelan <shelandebesai@gmail.com>
2018-10-02 17:42:40 -0700

    total revenue and rating methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -9,8 +9,16 @@ class Driver < ApplicationRecord
     total = 0
     trips.each do |trip|
 
-    total += (trip.cost - 1.65)
+      total += (trip.cost - 1.65)
     end
     return total_revenue = (total * 0.80)
+  end
+
+  def average_rating
+    total = 0
+    self.trips.each do |trip|
+      total += trip.rating
+    end
+    return total/trips.count.to_f
   end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,18 @@
+require 'date'
+
 class Passenger < ApplicationRecord
   has_many :trips
   has_many :drivers, through: :trips
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def assign_trip
+    trip = Trip.new
+    self.trips << trip
+    Driver.all.sample.trips << trip
+    trip.date = Date.today
+    trip.cost = Random.rand(0..9)
+    trip.save
+  end
 end
