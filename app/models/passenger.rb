0045b33f259dd88e08e475b13002eb5705a199f8
Amber Lynn <griffiam@gmail.com>
2018-10-03 16:54:04 -0700

    added total amount charged to each passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,12 @@
 class Passenger < ApplicationRecord
   has_many :trips
   has_many :drivers, through: :trips
+
+  def total_cost
+    total = 0
+    self.trips.each do |trip|
+      total += (trip.cost/100.to_f)
+    end
+    return total
+  end
 end
