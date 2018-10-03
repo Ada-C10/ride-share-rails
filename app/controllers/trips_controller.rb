1c4943ca93bc331653a39a82411578db8ba20253
Goeun Park <goeuntothepark@gmail.com>
2018-10-03 15:17:24 -0700

    add CRUD for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,4 +3,60 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+  end
+
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    result = @trip.update(trip_params)
+
+    if result
+      redirect_to trip_path(@trip.id)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def destroy
+    trip = Trip.find_by(id: params[:id])
+
+    trip.destroy
+    redirect_to trips_path
+  end
+
+
+
+  private
+  def trip_params
+    return params.require(:trip).permit(
+      :date,
+      :rating,
+      :cost
+    )
+  end
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,9 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  validates :date, presence: true
+  validates :rating, numericality: {only_integer: true, less_than_or_equal_to: 5, greater_than: 0}
+  validates :cost, numericality: {only_integer: true}
+
 end
