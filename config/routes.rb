ce7871666a8c6023adb455c6334bb42c54d37ba6
Carly Reiter <yepodacarly@gmail.com>
2018-10-03 14:52:03 -0700

    modified routes to accept Homes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #make a root page for home 
+  #make a root page for home
+  root 'homes#index'
+
+  get '/homes', to: 'homes#index'
 
   resources :drivers
   resources :trips
