e84c9050b2f0ce5deded8b39cb82d3c68813e39e
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-03 16:48:55 -0700

    added average rating method to model and view for driver.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,14 @@
 class Driver < ApplicationRecord
   has_many :trips
   has_many :passengers, through: :trips
+
+  def average_rating
+    counter = 0
+    rating = 0
+    self.trips.each do |trip|
+      rating += trip.rating
+      counter += 1
+    end
+    return (rating.to_f/counter).round(2) 
+  end
 end
