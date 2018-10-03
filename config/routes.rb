5ccfdc5bdc3056de073ec15a743260bfc855cdf2
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-02 23:24:07 -0700

    added comment in routes.rb for advice

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :drivers
   resources :passengers
   resources :trips
+  # custom route to create a new trip with params of driver and passenger id's?
 end
