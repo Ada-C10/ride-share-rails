876a896a6644f65535c37a593eec85e4887b5c97
kangazoom <michellelcronin@gmail.com>
2018-10-02 17:06:48 -0700

    added very basic CRUD functionality to passengers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,53 +3,60 @@ class PassengersController < ApplicationController
     # @passenger = Passenger.all.order(:due_date)
       @passengers = Passenger.all
   end
-  #
-  # def show
-  #   @task = Task.find(params[:id].to_i)
-  # end
-  #
-  # def new
-  #   @task= Task.new
-  # end
-  #
-  # def create
-  #   @task = Task.new(task_params)
-  #
-  #   is_successful_save = @task.save
-  #
-  #   if is_successful_save
-  #     redirect_to tasks_path
-  #   else
-  #     render :new
-  #   end
-  # end
-  #
-  # def edit
-  #   @task = Task.find_by(id: params[:id])
-  #
-  #   if !@task
-  #     return raise ActiveRecord::RecordNotFound, 'Record not found - cannot edit'
-  #   end
-  # end
-  #
-  # def update
-  #   @task = Task.find(params[:id])
-  #
-  #   @task.update(task_params)
-  #
-  #   redirect_to task_path(@task)
-  # end
-  #
-  # def destroy
-  #   @task = Task.find_by(id: params[:id])
-  #
-  #   if @task
-  #     @task.destroy
-  #     redirect_to tasks_path
-  #   else
-  #     return raise ActiveRecord::RecordNotFound, 'Record not found - cannot delete'
-  #   end
-  # end
+
+  def show
+    # TODO: add 404 routing if not found; make a method?
+    @passenger = Passenger.find(params[:id].to_i)
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    is_successful_save = @passenger.save
+
+    if is_successful_save
+      redirect_to passengers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # TODO: add 404 routing if not found; make a method?
+
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if !@passenger
+      return raise ActiveRecord::RecordNotFound, 'Record not found - cannot edit'
+    end
+  end
+
+  def update
+    # TODO: add 404 routing if not found; make a method?
+
+    @passenger = Passenger.find(params[:id])
+
+    @passenger.update(passenger_params)
+
+    redirect_to passenger_path(@passenger)
+  end
+
+  def destroy
+    # TODO: add 404 routing if not found; make a method?
+
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger
+      @passenger.destroy
+      redirect_to passengers_path
+    else
+      return raise ActiveRecord::RecordNotFound, 'Record not found - cannot delete'
+    end
+  end
   #
   # def complete
   #   @task = Task.find(params[:id].to_i)
@@ -63,13 +70,12 @@ class PassengersController < ApplicationController
   #   redirect_to tasks_path
   # end
   #
-  # private
-  #
-  # def task_params
-  #   return params.require(:task).permit(
-  #     :name,
-  #     :description,
-  #     :due_date,
-  #   )
-  # end
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(
+      :name,
+      :phone_num,
+    )
+  end
 end
