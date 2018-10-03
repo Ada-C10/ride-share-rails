fc0617800ac10967531a9261c4e5c02fa2fddf87
Addison Cole <addison.cole85@gmail.com>
2018-10-03 16:16:50 -0700

    add driver rating method in model and erb in view

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,17 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_revenue
+    return self.trips.reduce(0) do |sum, trip|
+      sum + ((trip.cost - 165)* 0.8)
+    end
+  end
+
+  def average_rating
+    total = self.trips.reduce(0) do |sum, trip|
+      sum + trip.rating
+    end
+    return (total / self.trips.length)
+  end
 end
