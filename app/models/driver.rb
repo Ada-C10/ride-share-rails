853d72620d948cd1f2829ffb4174e38773d2a55c
Jazz <jazziesf21@yahoo.com>
2018-10-02 17:23:17 -0700

    added a trip history to passenger

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,7 +1,18 @@
 class Driver < ApplicationRecord
-  has_many :trips
-  has_many :passengers, through: :trips
+ has_many :trips
+ has_many :passengers, through: :trips
 
-  validates :name, presence: true
-  validates :vin, presence: true
+ validates :name, presence: true
+ validates :vin, presence: true
+
+ def total_revenue
+   total = 0
+   trips.each do |trip|
+
+   total += (trip.cost - 1.65)
+   end
+   return total_revenue = (total * 0.80)
+ end
+
+
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,5 +5,16 @@ class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_num, presence: true
 
-  
+  def total_spent
+    total = 0
+    trips.each do |trip|
+    total += trip.cost
+    end
+    return total
+  end
+
+  def all_passenger_trips
+    trips.all
+  end
+
 end
