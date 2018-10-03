f8d0c4e531176f0b57da2f6dc672c9cd81338e4c
Lindsay Terchin <lterchin@gmail.com>
2018-10-03 08:48:39 -0700

    created model to calculate the average driver rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,15 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def average_rating
+    total_rating = 0.0
+
+    trips.each do |trip|
+      total_rating += trip.rating.to_i
+    end
+
+    average_rating = total_rating / trips.length
+
+    return average_rating
+  end
 end
