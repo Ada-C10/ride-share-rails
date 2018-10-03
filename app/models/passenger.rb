9914e221b679688211ac46b07bd3e48c1172d66e
Addison Cole <addison.cole85@gmail.com>
2018-10-03 15:24:46 -0700

    passenger view shows total expenditures of all trips

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,11 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_expenditures
+    return self.trips.reduce(0) do |sum, trip|
+      sum + trip.cost
+    end
+  end
+
 end
