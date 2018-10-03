2f6f23c31a9bd380da45888844c4d2b0f0fd4a1e
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-02 17:14:41 -0700

    add status to passengers and set the defalut value to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_174837) do
+ActiveRecord::Schema.define(version: 2018_10_03_000949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_174837) do
     t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: true
   end
 
   create_table "trips", force: :cascade do |t|
