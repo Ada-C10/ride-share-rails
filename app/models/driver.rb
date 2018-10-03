feff1d7cf14422047da39a76b7ce018e3c99c556
Goeun Park <goeuntothepark@gmail.com>
2018-10-03 16:47:33 -0700

    add business logic for driver's rating based on trips

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,4 +4,20 @@ class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, uniqueness: true
 
+  def rating
+    num_trips = self.trips.length
+    rating = 0
+
+    num_trips.times do |i|
+      rating += self.trips[i].rating
+    end
+
+    rating = rating/num_trips
+    return rating
+  end
+
+  def earned
+  end
+
+
 end
