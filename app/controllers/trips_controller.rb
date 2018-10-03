b2ceffe66c7e38da3e3aac7fc18556bc5b163cb6
Semret Nicodimos <snicodimos@gmail.com>
2018-10-03 14:04:18 -0700

    minor changes to trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,29 @@ class TripsController < ApplicationController
   def index
 
   end
+
+
+  def new
+
+  end
+
+
+  def create
+
+
+  end
+
+
+
+
+  private
+  def trip_params
+    return params.require(:trip).permit(
+      :date,
+      :driver_id,
+      :passenger_id,
+      :price,
+      :rating,
+    )
+  end
 end
