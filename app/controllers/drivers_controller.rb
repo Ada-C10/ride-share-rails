e69e37d6b2c40e2b436e1e50121b15c01275297f
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-02 20:57:18 -0700

    Created migration for car_make and car_model

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,33 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.all
   end
+
+  def show
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(
+      :name,
+      :vin,
+      :car_make,
+      :car_model
+    )
+  end
 end
