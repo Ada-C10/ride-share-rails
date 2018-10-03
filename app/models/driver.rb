90e6e838178bd4b51bc066569ed9d75d9fe05642
Layla <layla.bristol@gmail.com>
2018-10-02 22:26:51 -0700

    adjusted some formatting

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,15 +5,6 @@ class Driver < ApplicationRecord
   validates :vin, presence: true
 
 
-
-  def first_published
-    books_with_year = self.books.where.not(pub_date: nil)
-    first_book = books_with_year.order(pub_date: :asc).first
-    return first_book ? first_book.pub_date : nil
-  end
-
-
-
   def average_rating
     #self is same thing as driver instance, since we are in model
     sum = 0
@@ -28,16 +19,6 @@ class Driver < ApplicationRecord
 
     average = sum / length
     return average
-
-
-
-    # driver = Driver.find(driver_id)
-
-
-    # drivers.each do |driver|
-    #   average_rating = driver.trips.rating.sum / driver.trips
-    #   return average_rating
-    # end
   end
 
 end
