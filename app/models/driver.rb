1b7225b7d797a2285f7f513c3ecc78db736eb14c
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-02 20:30:21 -0700

    Change method name

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,7 +3,7 @@ class Driver < ApplicationRecord
   validates :name, presence: true, format: {with: /\A[a-zA-Z\.\'\-]{2,50}(?: [a-zA-Z\.\'\-]{2,50})+\Z/}
   validates :vin, presence: true, format: {with: /[A-HJ-NPR-Z0-9]/i}, length: { is: 17 }
 
-  def change_status
+  def becomes_unavailable
     self.toggle(:is_available?).save
   end
 
