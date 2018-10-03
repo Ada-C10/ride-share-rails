a4114c4cf7152ef51a3f6248b1ccf0ff4f6b03b0
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-03 15:25:30 -0700

    Created method to show amount passenger has been charged. Updated passenger show view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,15 @@ class Passenger < ApplicationRecord
   has_many :trips
 
   validates :name, presence: true
-  validates :phone_num, presence: true 
+  validates :phone_num, presence: true
+
+  def total_charged
+    charged = 0
+    self.trips.each do |trip|
+      charge = trip.convert_money(trip.cost)
+      charged += charge 
+    end
+    return charged
+  end
+
 end
