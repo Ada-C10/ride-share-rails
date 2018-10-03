312c14fa3d8b865083d939323118ec2891895882
Hannah <hannahvanseeters@gmail.com>
2018-10-02 18:34:30 -0700

    added validation on vin name and all trip attributes

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+  #can you write these all in one line instead of individually?
+  vaidates :passenger_id, presence: true
+  validates :driver_id, presence: true
+  validates :date, presence: true
+  validates :rating, presence: true
+  validates :cost, presence: true
 end
