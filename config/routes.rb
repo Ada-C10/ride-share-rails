76ba8eba2c2688b7380203cb88d7b5fe7a213f41
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-03 14:48:22 -0700

    typo fix for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :drivers
   resources :passengers
-  resources :trips, except: :create, :new
+  resources :trips, except: [:create, :new]
 
   resources :passengers do
     resources :trips, only: [:create]
