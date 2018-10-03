9cea2a31f821c10f468e7b3ae08e8a881f5d4bd2
Hannah <hannahvanseeters@gmail.com>
2018-10-03 14:42:23 -0700

    fixed trip validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
   #can you write these all in one line instead of individually?
-  vaidates :passenger_id, presence: true
+  validates :passenger_id, presence: true
   validates :driver_id, presence: true
   validates :date, presence: true
   validates :rating, presence: true
