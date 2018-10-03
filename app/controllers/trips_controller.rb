f93589a9c50d5fccd5397bc0a1d610d9e12abcec
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-03 16:17:29 -0700

    Using nested route /drivers/:id/trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,10 @@
 class TripsController < ApplicationController
+  def index
+    if params[:driver_id]
+      @driver = Driver.find_by(id: params[:driver_id])
+      @trips = @driver.trips
+    else
+      @trips = Trip.all
+    end
+  end
 end
