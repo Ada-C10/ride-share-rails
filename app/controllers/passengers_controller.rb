34e46dc752c8997e45487be8054a2729b7e15aa2
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-02 17:07:12 -0700

    complete the actions in passenger.rb and related erb files

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,4 +1,58 @@
 class PassengersController < ApplicationController
   def index
+  @passengers = Passenger.all.order(:id)
+end
+
+def show
+  id = params[:id].to_i
+  @passenger = Passenger.find_by(id: id)
+
+  if @passenger.nil?
+   render :notfound, status: :not_found
   end
+end
+
+def new
+  @passenger = Passenger.new
+end
+
+def edit
+  @passenger = Passenger.find(params[:id].to_i)
+end
+
+def destroy
+  passenger = Passenger.find_by(id: params[:id].to_i)
+
+  begin
+   @deleted_passenger = passenger.destroy
+   redirect_to passengers_path
+  rescue
+    render  :deletewarning
+  end
+
+end
+
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    passenger = Passenger.find_by(id: params[:id].to_i)
+    passenger.update(passenger_params)
+
+    redirect_to passenger_path(passenger.id)
+  end
+
+private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
