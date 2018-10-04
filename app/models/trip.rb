bffd76d6f7d02987ef843c5e0909403ae3c47345
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-03 23:44:02 -0700

    Added links to Driver and Passenger on Trip details page

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,14 +2,6 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  def driver_name
-    return self.driver.name
-  end
-
-  def passenger_name
-    return self.passenger.name
-  end
-
   def status
     if self.rating == nil
       return :in_progress
