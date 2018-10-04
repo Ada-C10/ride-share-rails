a442c2f41360b8419dcfac3f0c5a424e1381d0ac
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-04 13:41:05 -0700

    add trip controller actions

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,5 @@
 class TripsController < ApplicationController
   def index
-
-    if params[:passenger_id]
-      @passenger = Passenger.find_by(id: params[:passenger_id])
-      @trips = @passenger.trips
-
     if params[:driver_id]
       @user = Driver.find_by(id: params[:driver_id])
       @trips = @user.trips
@@ -13,22 +8,22 @@ class TripsController < ApplicationController
       @user = Passenger.find_by(id: params[:passenger_id])
       @trips = @user.trips
 
-
     else
       @trips = Trip.all
     end
   end
 
-
   def show
     if params[:driver_id]
-      user = Driver.find_by(id: params[:driver_id])
-      @trip = user.trips.find_by(params[:trip_id])
+      @user = Driver.find_by(id: params[:driver_id])
+      @trips = @user.trip.find_by(params[:driver_id])
 
     elsif params[:passenger_id]
-      user = Passenger.find_by(id: params[:passenger_id])
-      @trip = user.trips.find_by(params[:trip_id])
+      @user = Passenger.find_by(id: params[:passenger_id])
+      @trips = @user.trips
 
+    else
+      @trips = Trip.all
     end
 
   end
@@ -61,5 +56,4 @@ def trip_params
   return params.require(:trip).permit(
     :driver_id, :passenger_id, :date, :rating, :cost
   )
-
 end
