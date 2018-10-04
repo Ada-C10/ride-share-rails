27865e0bab719a83b459ca83ba283875b372ca06
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-03 20:46:20 -0700

    add associated trips with drivers and passengers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,8 +1,13 @@
 class TripsController < ApplicationController
   def index
     if params[:driver_id]
-      @driver = Driver.find_by(id: params[:driver_id])
-      @trips = @driver.trips
+      @user = Driver.find_by(id: params[:driver_id])
+      @trips = @user.trips
+
+    elsif params[:passenger_id]
+      @user = Passenger.find_by(id: params[:passenger_id])
+      @trips = @user.trips
+
     else
       @trips = Trip.all
     end
