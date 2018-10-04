7e97ceae41da085024de8eaf1ebdfaba087fa969
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-04 00:56:00 -0700

    add helper method to format costs and average ratings on driver, passenger, and trip views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  include ActionView::Helpers::NumberHelper
+  def currency(total)
+    total *= 0.01
+    return number_to_currency(total)
+  end
+
+  def rating(rating)
+    return number_with_precision(rating, precision: 1)
+  end
 end
