f606854e329f949f041112c5f629fbd84118d6f4
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-03 22:31:40 -0700

    Calculating avg_trip_rating for each driver

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,6 +3,8 @@ class TripsController < ApplicationController
     if params[:driver_id]
       @driver = Driver.find_by(id: params[:driver_id])
       @trips = @driver.trips
+
+      @avg_trip_rating = avg_trip_rating(@trips)
     else
       @trips = Trip.all
     end
@@ -11,5 +13,19 @@ class TripsController < ApplicationController
   def show
     trip_id = params[:id]
     @trip = Trip.find_by(id: trip_id)
+  end
+
+  def avg_trip_rating(trips)
+    num_of_trips = trips.count
+    trip_rating_array = trips.map do |trip|
+      trip.rating
+    end
+
+    sum_of_ratings = 0
+    trip_rating_array.each do |rating|
+      sum_of_ratings += rating
+    end
+
+    return sum_of_ratings / num_of_trips
   end
 end
