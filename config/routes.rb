64f6f5b1b521ee346c023d7903bea0556ad0d04e
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-04 15:34:44 -0700

    Changing resources: drivers

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :passengers, :drivers, :trips
+
+  resources :passengers, :trips, :drivers
+
+
+
   resources :drivers do
     resources :trips, include: [:index, :show]
   end
+
+  
 
 end
