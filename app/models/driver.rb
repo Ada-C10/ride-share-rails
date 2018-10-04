35c3a5f032cce8d6d640857492b7b6d19ce6782b
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-04 10:00:36 -0700

    In-progress driver correctly displays avg rating and tot earnings

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,27 +3,31 @@ class Driver < ApplicationRecord
 
   def total_earnings
   #The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
-    if self.is_available? == false
-      return nil
-    else
-      total_cost = self.trips.reduce(0) { |sum, trip| sum + trip.cost }
-      num_trips = self.trips.length
-      total_earnings = ((total_cost - (num_trips * 1.65)) * 0.8) / 100
-      return total_earnings.round(2)
-    end
+    total_cost = self.trips.reduce(0) { |sum, trip|
+      if trip.cost == nil
+        sum + 0
+      else
+        sum + trip.cost
+      end
+      }
+    num_trips = self.trips.length
+    total_earnings = ((total_cost - (num_trips * 1.65)) * 0.8) / 100
+    return total_earnings.round(2)
   end
 
   def average_rating
-    if self.is_available? == false
-      return "- in-progress -" # TODO: discount nil trips and return a rating
-    else
-      sum_rating = self.trips.reduce(0) { |sum, trip| sum + trip.rating }
-      num_trips = self.trips.length.to_f
-      if num_trips == 0
-        return "- no trips -"
+    sum_rating = self.trips.reduce(0) { |sum, trip|
+      if trip.rating == nil
+        sum + 0
       else
-        return '%.2f' % "#{sum_rating/num_trips}"
+        sum + trip.rating
       end
+    }
+    num_trips = self.trips.length.to_f
+    if num_trips == 0
+      return "- no trips -"
+    else
+      return '%.2f' % "#{sum_rating/num_trips}"
     end
   end
 
