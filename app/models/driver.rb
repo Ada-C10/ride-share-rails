70fff6b596d5f37f173ead66ccb44f031b5a954a
Naheed Arang <arang.na@gmail.com>
2018-10-04 16:14:36 -0700

    refactor methods

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,41 +2,38 @@ class Driver < ApplicationRecord
   has_many :trips
 
   def all_trips
-    @all_trips = []
-    @all_trips << Trip.where(driver_id: id)
-    return @all_trips
+    temp_trips = []
+    all_trips = []
+    temp_trips << Trip.where(driver_id: id)
+    temp_trips.each do |trips|
+      trips.each do |trip|
+        all_trips << trip
+      end
+    end
+    return all_trips
   end
 
   def average_rating
-    rides = all_trips
+    trips = all_trips
     all_ratings = []
 
-    rides.each do |ride|
-      ride.each do |individual|
-
-        if individual.rating != nil
-          all_ratings << individual.rating
-        end
+    trips.each do |trip|
+      if trip.rating != nil
+        all_ratings << trip.rating
       end
     end
     sum_ratings = all_ratings.sum
     average_rating = sum_ratings / all_ratings.length
     return average_rating
-
   end
 
   def total_earnings
-    rides = all_trips
+    trips = all_trips
     profit_array = []
 
-    rides.each do |ride|
-      ride.each do |individual|
-
-        if individual.cost != nil
-          temp = individual.cost - 1.65
-          profit_array << temp * 0.8
-        end
-      end
+    trips.each do |trip|
+      temp = trip.cost - 1.65
+      profit_array << temp * 0.8
     end
     return profit_array.sum
   end
