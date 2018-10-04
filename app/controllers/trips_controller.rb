7e7f015ecf96a3004da4fcb04b5eea6ded334ebf
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-04 16:04:42 -0700

    Moving business logic from controller to model

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,8 +4,6 @@ class TripsController < ApplicationController
       @driver = Driver.find_by(id: params[:driver_id])
       @trips = @driver.trips
 
-      @avg_trip_rating = avg_trip_rating(@trips)
-      @total_trip_earnings = total_trip_earnings(@trips)
     else
       @trips = Trip.all
     end
@@ -40,36 +38,6 @@ class TripsController < ApplicationController
     redirect_to driver_trips_path(trip.driver_id)
   end
 
-  def avg_trip_rating(trips)
-    num_of_trips = trips.count
-
-    trip_rating_array = trips.map do |trip|
-      trip.rating
-    end
-
-    sum_of_ratings = 0
-    trip_rating_array.each do |rating|
-      sum_of_ratings += rating
-    end
-
-    return sum_of_ratings / num_of_trips
-  end
-
-  def total_trip_earnings(trips)
-    total_num_of_trips = trips.count
-
-    trip_cost_array = trips.map do |trip|
-      (trip.cost.to_f) / 100
-    end
-
-    sum_of_all_trips = 0
-    trip_cost_array.each do |cost|
-      sum_of_all_trips += cost
-    end
-
-    revenue = (sum_of_all_trips - (total_num_of_trips * 1.65)) * 0.8
-    return revenue.round(2)
-  end
 
   private
   def trip_params
