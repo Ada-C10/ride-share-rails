8df013e77066a7eb595d55972d1bf4e6dfb7d71d
Katricia Smith <smithkatricia@gmail.com>
2018-10-04 14:57:58 -0700

    Add model relationship and input validation. Add average_rating and total_earnings methods for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,13 @@
 class Driver < ApplicationRecord
+  has_many :trips
+  validates :name, presence: true
+  validates :vin, presence: true, uniqueness: true, length: {is: 17}
+
+  def average_rating
+    trips.sum(&:rating)/ trips.length
+  end
+
+  def total_earnings
+    trips.sum(&:cost)
+  end
 end
