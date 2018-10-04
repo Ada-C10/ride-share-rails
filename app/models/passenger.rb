5ea18f288624b779e0106e1767244942f5a7215e
Kat Perreira <kat.perreira@gmail.com>
2018-10-04 16:06:02 -0700

    added business logic for passenger, also added links to create a new passenger and a new driver in the nav page

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,12 +1,26 @@
 class Passenger < ApplicationRecord
   has_many :trips
 
-def all_trips
-  @all_trips = []
-  @all_trips << Trip.where(passenger_id: id)
-  return @all_trips
-end
+  def all_trips
+    @all_trips = []
+    @all_trips << Trip.where(passenger_id: id)
+    return @all_trips
+  end
 
+  def total_spent
+    rides = all_trips
+    charges_array = []
+
+    rides.each do |ride|
+      ride.each do |individual|
+
+        if individual.cost != nil
+          charges_array << individual.cost
+        end
+      end
+    end
+    return charges_array.sum
+  end
 
 
 end
