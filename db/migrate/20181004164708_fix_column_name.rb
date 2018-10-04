dac28eb153f15795f65144d5d0601a4b0f6bf6c8
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-04 14:53:03 -0700

    Change names

--- db/migrate/20181004164708_fix_column_name.rb
+++ db/migrate/20181004164708_fix_column_name.rb
@@ -1,5 +1,5 @@
 class FixColumnName < ActiveRecord::Migration[5.2]
   def change
-    rename_column :drivers, :is_available?, :is_available
+    rename_column :drivers, :is_available, :is_available
   end
 end
