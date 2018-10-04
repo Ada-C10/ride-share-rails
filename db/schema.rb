81c51a39734d9a3abebc6586ce8650e05d09bb3c
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-04 09:48:21 -0700

    Rename attribute without question mark

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_210309) do
+ActiveRecord::Schema.define(version: 2018_10_04_164708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_210309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
-    t.boolean "is_available?", default: true
+    t.boolean "is_available", default: true
   end
 
   create_table "passengers", force: :cascade do |t|
