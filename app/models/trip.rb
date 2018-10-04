a88340ccdbd8da689d6740e0193c9956f81fed68
Daniela Di Iorio <danielad@gmail.com>
2018-10-04 13:56:01 -0700

    rating validation allows nil

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ApplicationRecord
   validates :driver_id, :passenger_id, :date, :cost, presence: true
-  validates :rating, :inclusion => {:in => 1..5}
+  validates :rating, :inclusion => {:in => [nil, 1..5]}
+  validates :cost, numericality: true
 
   belongs_to :driver
   belongs_to :passenger
