9e5a82f9ce81a2ead4616eced113d219892de9e6
Kat Perreira <kat.perreira@gmail.com>
2018-10-04 14:43:56 -0700

    fixed homepage situation, new bugs in trips all

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,21 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+def all_trips
+  @all_trips = []
+  @all_trips << Trip.where(@passenger.id)
+  return @all_trips
+end
+
+
+
+
+
+
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # assigning temp root to the drivers page, replace with the homepage once its set up
-  root 'passengers#index'
+  root 'homepage#index'
+
+  resources :homepage, only: [:index]
 
 
   resources :trips
