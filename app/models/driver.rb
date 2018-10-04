4bf4c3c18277024195253e1ab8ab3e4e7cfef4c7
Maryam Shitu <maryamshitu@gmail.com>
2018-10-04 12:24:47 -0700

    Trips added to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,7 +1,16 @@
 class Driver < ApplicationRecord
   has_many :trips
+  # validates :name, presence: true
+  # validates :vin, presence: true
 
   def self.next_available
     return Driver.where.not(id: Trip.where(rating: nil).select(:driver_id)).order("RANDOM()").first
   end
+
+  def self.total_earnings
+  end
+
+  def self.average_rating
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'layouts#application'
 
-  resources :passengers, :drivers, :trips
-  #
-  # resources:authors do
-  #   resources :books, only :[index, :new]
+  resources :trips
+  resources :passengers, :drivers do
+    resources :trips
+  end
 
 end
