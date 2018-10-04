63fa8534149ad290d3f12ad25e3a4eafff4da921
Aurea <fu.sakura@gmail.com>
2018-10-04 13:56:12 -0700

    remove embedded trip index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: 'trips#index'
 
   resources :drivers
-
-  resources :passengers do
-    resources :trips, only: [:index, :create]
-  end
-
   resources :trips
+  
+  resources :passengers do
+    resources :trips, only: [:create]
+  end
 
   post '/trips/:id', to: 'trips#rated'
 
