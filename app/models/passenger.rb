aa9ae71ae82db5be9cefa68ad946fd5de0a73844
Maryam Shitu <maryamshitu@gmail.com>
2018-10-04 17:09:37 -0700

    new passenger validation added

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :trips
-  # validates :name, presence: true
-  # validates :phone_num, presence: true
+  validates :name, presence: true
+  validates :phone_num, presence: true
 
   def total_trips_cost
     total_cost = 0
