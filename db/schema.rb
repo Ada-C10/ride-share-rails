fe38df849762d16973b1405276f1616878b29c37
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-04 20:17:51 -0700

    Added a default status of active to drivers and passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_173206) do
+ActiveRecord::Schema.define(version: 2018_10_05_031648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_173206) do
     t.string "vin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "active"
   end
 
   create_table "passengers", force: :cascade do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_10_02_173206) do
     t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "active"
   end
 
   create_table "trips", force: :cascade do |t|
