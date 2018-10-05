8a78b401266003b3b7abd0bc77252b96d929e031
Trang <trang.frego@gmail.com>
2018-10-05 10:10:32 -0700

    complete validations errors for Drivers and Passengers

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,7 +2,7 @@ class Passenger < ApplicationRecord
   has_many :trips
 
   validates :name, presence: true
-  validates :phone_num, presence: true, uniqueness: true
+  validates :phone_num, presence: true, uniqueness: true, length: { in: 10..20 }
 
   def total_charged
     # if self.trip.cost  !=nil
