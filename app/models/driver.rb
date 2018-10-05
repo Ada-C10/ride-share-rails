ca53a0ccede3d64ebb57e620c245c0489f685d1d
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-04 17:52:27 -0700

    add total earnings to driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,11 +1,24 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+
   def average_rating
     driver_trips = self.trips.where.not(rating: nil)
 
     sum = driver_trips.sum { |t| t.rating }.to_f
 
     return sum / driver_trips.length
+  end
+
+  def total_earnings
+    driver_cut = 0.8
+
+    trips_cost = self.trips.where.not(cost: nil)
+
+    earnings_minus_fee = trips_cost.map { |t| t.cost - 165 }
+
+    calculated_earnings = earnings_minus_fee.reduce(:+)
+
+    return calculated_earnings * driver_cut
   end
 end
