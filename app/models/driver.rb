a188a282ce9ac9b160758fccbecb5346be291eb5
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-04 17:14:48 -0700

    add driver rating to trips

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,11 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def average_rating
+    driver_trips = self.trips.where.not(rating: nil)
+
+    sum = driver_trips.sum { |t| t.rating }.to_f
+
+    return sum / driver_trips.length
+  end
 end
